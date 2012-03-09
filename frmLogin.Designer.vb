bd523723017b4155003023d485243ed88f0e2533
Ruel Pagayon <ruel@ruel.me>
2012-03-09 13:22:28 +0800

    Initial Uncompleted Project

---
+++ frmLogin.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LogoPictureBox = New System.Windows.Forms.PictureBox
+        Me.UsernameLabel = New System.Windows.Forms.Label
+        Me.PasswordLabel = New System.Windows.Forms.Label
+        Me.txtUser = New System.Windows.Forms.TextBox
+        Me.txtPass = New System.Windows.Forms.TextBox
+        Me.btnOK = New System.Windows.Forms.Button
+        Me.btnCancel = New System.Windows.Forms.Button
+        Me.llFg = New System.Windows.Forms.LinkLabel
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LogoPictureBox
+        '
+        Me.LogoPictureBox.Image = Global.Gradon.My.Resources.Resources.gradon_tiny
+        Me.LogoPictureBox.Location = New System.Drawing.Point(27, 23)
+        Me.LogoPictureBox.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.LogoPictureBox.Name = "LogoPictureBox"
+        Me.LogoPictureBox.Size = New System.Drawing.Size(138, 201)
+        Me.LogoPictureBox.TabIndex = 0
+        Me.LogoPictureBox.TabStop = False
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.Location = New System.Drawing.Point(183, 23)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(257, 28)
+        Me.UsernameLabel.TabIndex = 0
+        Me.UsernameLabel.Text = "&User ID"
+        Me.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.Location = New System.Drawing.Point(183, 94)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(257, 28)
+        Me.PasswordLabel.TabIndex = 2
+        Me.PasswordLabel.Text = "&Password"
+        Me.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'txtUser
+        '
+        Me.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtUser.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtUser.Location = New System.Drawing.Point(185, 48)
+        Me.txtUser.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(256, 23)
+        Me.txtUser.TabIndex = 1
+        '
+        'txtPass
+        '
+        Me.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPass.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtPass.Location = New System.Drawing.Point(185, 118)
+        Me.txtPass.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(256, 23)
+        Me.txtPass.TabIndex = 3
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(201, 196)
+        Me.btnOK.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(110, 28)
+        Me.btnOK.TabIndex = 4
+        Me.btnOK.Text = "&OK"
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(321, 196)
+        Me.btnCancel.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(110, 28)
+        Me.btnCancel.TabIndex = 5
+        Me.btnCancel.Text = "&Cancel"
+        '
+        'llFg
+        '
+        Me.llFg.ActiveLinkColor = System.Drawing.Color.DarkGray
+        Me.llFg.AutoSize = True
+        Me.llFg.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.llFg.LinkColor = System.Drawing.Color.Gray
+        Me.llFg.Location = New System.Drawing.Point(272, 146)
+        Me.llFg.Name = "llFg"
+        Me.llFg.Size = New System.Drawing.Size(88, 13)
+        Me.llFg.TabIndex = 6
+        Me.llFg.TabStop = True
+        Me.llFg.Text = "Forgot Password"
+        '
+        'frmLogin
+        '
+        Me.AcceptButton = Me.btnOK
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(468, 257)
+        Me.Controls.Add(Me.llFg)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Controls.Add(Me.LogoPictureBox)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmLogin"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Gradon - Login"
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents llFg As System.Windows.Forms.LinkLabel
+
+End Class
